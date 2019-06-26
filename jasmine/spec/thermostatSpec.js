d8b6ed5bec6018120fe78e4d618945cb34ab350c
Celia <celiafrom12@gmail.com>
2019-06-26 14:47:22 +0100

    refactored variable declaration

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -1,15 +1,17 @@
 describe ('Thermostat', function(){
 
   var thermostat;
+  
+  beforeEach(function(){
+    thermostat = new Thermostat;
+  });
 
   it('starts at 20 degrees', function(){
-    thermostat = new Thermostat;
     expect(thermostat.temp).toEqual(20);
   });
 
   describe('increase temperature', function() {
     it('increase by 1', function() {
-      thermostat = new Thermostat;
       thermostat.increase();
       expect(thermostat.temp).toEqual(21);
     });
@@ -17,9 +19,8 @@ describe ('Thermostat', function(){
 
   describe('decrease temperature', function() {
     it('decreases by 1', function() {
-      thermostat = new Thermostat;
       thermostat.decrease();
       expect(thermostat.temp).toEqual(19);
-    })
-  })
+    });
+  });
 });
